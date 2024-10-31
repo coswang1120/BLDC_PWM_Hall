1cb2de1394ce12ecba58e430c3d6fd5054cf1cf5
coswang1120 <coswang1120@gmail.com>
2024-10-31 16:36:59 +0800

    修正 preintf 發不出 (發現是模組現有問題) & 修正Control_Mode==1 不會動 (發現是之前 是需要接受命令)

---
+++ User_include/hello_world.h
@@ -0,0 +1,8 @@
+#ifndef __HELLO_WORLD_H
+#define __HELLO_WORLD_H
+#include "stm32f10x.h"
+
+
+
+
+#endif /* __HELLO_WORLD_H */
---
+++ User_include/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f10x.h"
+
+int fputc( int ch , FILE *f); //printf
+
+#endif /* __MAIN_H */
